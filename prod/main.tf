f94df65ddca84fb71ad6cc968e9408129b5f3ad2
hooryash <hooryash@gmail.com>
2020-01-25 20:13:53 +0500

    vpc module updated

--- prod/main.tf
+++ prod/main.tf
@@ -1,8 +1,8 @@
-module "vicks-vpc-dev" {
+module "vicks-vpc-prod" {
         source     = "../modules/vpc"
         vpc_cidr   = "192.168.0.0/16"
         tenancy    = "default"
-        vpc_id     = "${module.vicks-vpc-dev.vpc_id}"
+        vpc_id     = "${module.vicks-vpc-prod.vpc_id}"
         main_pub_sb = "192.168.1.0/24"
 
 }
@@ -12,6 +12,6 @@ module "vicks-ec2-dev" {
         EC2_COUNT   = "1"
         AMIS        = "ami-04b9e92b5572fa0d1"
         AWS_INSTANCE_TYPE = "t2.micro"
-        AWS_SUBNET_ID   = "${module.vicks-vpc-dev.subnet_id}"
+        AWS_SUBNET_ID   = "${module.vicks-vpc-prod.subnet_id}"
 }
 
